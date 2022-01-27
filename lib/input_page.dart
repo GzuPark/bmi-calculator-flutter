16cebce608c621d377dfafb739cca26a7041314b
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-27 17:01:50 +0900

    Split widgets from the input page: IconContent, ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
 const activeCardColor = Color(0xFF1d1e33);
 const bottomContainerColor = Color(0xFFeb1555);
 const bottomContainerHeight = 80.0;
-const iconContentTextColor = Color(0xFF8d8e98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -73,53 +75,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  final IconData icon;
-  final String label;
-
-  IconContent({
-    required this.icon,
-    required this.label,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, size: 80.0),
-        const SizedBox(height: 15.0),
-        Text(
-          label,
-          style: TextStyle(fontSize: 18.0, color: iconContentTextColor),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color color;
-  final Widget? cardChild;
-
-  ReusableCard({
-    required this.color,
-    this.cardChild,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: cardChild,
     );
   }
 }
