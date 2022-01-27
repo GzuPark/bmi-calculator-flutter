a816b2ab94906c17f2377bcd92e5d796b1ccb03a
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-27 16:55:37 +0900

    Apply reusable icon content widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const activeCardColor = Color(0xFF1d1e33);
 const bottomContainerColor = Color(0xFFeb1555);
 const bottomContainerHeight = 80.0;
+const iconContentTextColor = Color(0xFF8d8e98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -24,11 +26,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     color: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     color: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
                   ),
                 ),
               ],
@@ -67,11 +77,38 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  IconContent({
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80.0),
+        const SizedBox(height: 15.0),
+        Text(
+          label,
+          style: TextStyle(fontSize: 18.0, color: iconContentTextColor),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color color;
+  final Widget? cardChild;
 
   ReusableCard({
     required this.color,
+    this.cardChild,
   });
 
   @override
@@ -82,6 +119,7 @@ class ReusableCard extends StatelessWidget {
         color: color,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
